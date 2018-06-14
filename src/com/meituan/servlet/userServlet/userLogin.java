f7d7abd44e5350c7b360b4f4100d9204ba7a0751
anban9616 <2290838597@qq.com>
2018-06-14 15:46:40 +0800

    修改并测试登录

--- src/com/meituan/servlet/userServlet/userLogin.java
+++ src/com/meituan/servlet/userServlet/userLogin.java
@@ -5,23 +5,29 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.jsp.JspWriter;
-
 import com.meituan.dao.UserDAO;
 import com.meituan.dao.impl.UserDAOImpl;
+import com.meituan.domain.User;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Method;
 
 /**
- * Servlet implementation class LoginCheck
+ * ud : UserDAO
+ * doPost() : post请求
+ * check() : 响应check.ul ,ul即userLogin
+ * reg() : 响应reg.ul ,ul即userLogin
+ * phone() : 响应phone.ul ,ul即userLogin
+ * @author zhou
+ *
  */
 public class userLogin extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
+
+	private UserDAO ud = new UserDAOImpl();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
@@ -30,10 +36,10 @@ public class userLogin extends HttpServlet
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		// 1. 获取 ServletPath: /edit.do 或 /addCustomer.do
+		// 1. 获取 ServletPath: /check.ul或 reg.ul 等。 ul是userLogin的首字母缩写
 		String servletPath = request.getServletPath();
-
-		// 2. 去除 / 和 .do, 得到类似于 edit 或 addCustomer 这样的字符串
+		System.out.println(servletPath);
+		// 2. 去除 / 和 .ul, 得到类似于 check 或 reg 这样的字符串
 		String methodName = servletPath.substring(1);
 		methodName = methodName.substring(0, methodName.length() - 3);
 
@@ -51,26 +57,77 @@ public class userLogin extends HttpServlet
 			response.sendRedirect("error.jsp");
 		}
 	}
-	private UserDAO ud = new UserDAOImpl();
-	private void check(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException
+
+	private void check(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		PrintWriter out = response.getWriter();
 		String userPhone = request.getParameter("userPhone");
 		String userPass = request.getParameter("userPass");
-//		System.out.println(userPhone);
-//		System.out.println(userPass);
+		System.out.println(request.getServletPath() + ":" + userPhone);
+		System.out.println(request.getServletPath() + ":" + userPass);
 		long count = ud.getCountForUser(userPhone, userPass);
 		JSONArray jsonList = new JSONArray();
-		JSONObject jsonUser = new JSONObject();
-		if(count==1)
+		JSONObject jsonObj = new JSONObject();
+		if (count == 1)
 		{
-			jsonUser.put("checked", "true");
-		}
-		else 
+			jsonObj.put("checked", "true");
+		} else
 		{
-			jsonUser.put("checked", "false");
+			jsonObj.put("checked", "false");
 		}
-		jsonList.add(jsonUser);
+		jsonList.add(jsonObj);
+		out.println(jsonList);
+	}
+
+	private void reg(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		PrintWriter out = response.getWriter();
+		// 获取传递过来的参数
+		String userPhone = request.getParameter("userPhone");
+		String userPass = request.getParameter("userPass");
+		String userName = request.getParameter("userName");
+		System.out.println(request.getServletPath() + ":" + userPhone);
+		System.out.println(request.getServletPath() + ":" + userPass);
+		System.out.println(request.getServletPath() + ":" + userName);
+		// 查询数据库是否存在手机号
+		long count = ud.getCountForPhone(userPhone);
+		JSONArray jsonList = new JSONArray();
+		JSONObject jsonObj = new JSONObject();
+		if (count == 0)
+		{
+			// 未注册过的用户
+			User user = new User(userPhone, userPass, userName);
+			ud.sava(user);
+			jsonObj.put("checked", "true");
+		} else
+		{
+			// 注册过的用户
+			jsonObj.put("checked", "false");
+		}
+		jsonList.add(jsonObj);
+		out.println(jsonList);
+	}
+
+	private void phone(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		PrintWriter out = response.getWriter();
+		// 获取传递过来的参数
+		String userPhone = request.getParameter("userPhone");
+		System.out.println(request.getServletPath() + ":" + userPhone);
+		// 查询数据库是否存在手机号
+		long count = ud.getCountForPhone(userPhone);
+		JSONArray jsonList = new JSONArray();
+		JSONObject jsonObj = new JSONObject();
+		if (count == 0)
+		{
+			// 未注册过的手机号
+			jsonObj.put("checked", "true");
+		} else
+		{
+			// 注册过的手机号
+			jsonObj.put("checked", "false");
+		}
+		jsonList.add(jsonObj);
 		out.println(jsonList);
 	}
 }
