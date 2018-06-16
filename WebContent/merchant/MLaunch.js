0bd8111b0d768a6a9d8129979bce81809d49a978
Metaloe <metaloe@outlook.com>
2018-06-16 20:50:46 +0800

    big fix

---
+++ WebContent/merchant/MLaunch.js
@@ -0,0 +1,32 @@
+function upPicBtn(){
+    $("#uploadPic").click(function(){
+        $("#SPpic").click();
+    });
+}
+function checkInput(){
+    var text0=$("#SPname").val();
+    var text1=$("#SPdesc").val();
+    var text2=$("#SPprice").val();
+    var img0=$("#SPpic").val();
+    if(text0==null||text0==""
+        ||text1==null||text1==""
+        ||text2==null||text2==""
+        ||img0==null||img0==""){
+        $("#upload").attr("disabled","disabled");
+    }else{
+        $("#upload").removeAttr("disabled");
+    }
+}
+function toUpload(){
+    var options={
+		url:"../merchant/?",
+		type:"post",
+		dataType:"text",
+		success: showResponse
+    };
+    $("#launchForm").ajaxSubmit(options);
+    return false;
+}
+function showResponse(){
+    //div局部刷新
+}
