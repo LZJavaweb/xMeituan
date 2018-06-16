bd39797df89e2b8adae38ca53f00d958b03680e0
anban9616 <2290838597@qq.com>
2018-06-16 23:07:59 +0800

    完善页面命名

--- src/com/meituan/servlet/userServlet/userLookEvaluate.java
+++ src/com/meituan/servlet/userServlet/userLookEvaluate.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class userPage extends HttpServlet
+public class userLookEvaluate extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
 
